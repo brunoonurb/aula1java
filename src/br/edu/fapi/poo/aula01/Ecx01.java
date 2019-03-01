cd77e5cee4773d23e891b0997890a01834ec08aa
Aluno <Aluno@fapi.local>
2019-02-28 22:09:46 -0300

    mexi

--- src/br/edu/fapi/poo/aula01/Ecx01.java
+++ src/br/edu/fapi/poo/aula01/Ecx01.java
@@ -1,5 +1,16 @@
 package br.edu.fapi.poo.aula01;
 
-public class Ecx01 {
+import java.util.Scanner;
 
+public class Ecx01 {
+	public static void main(String[] args) {
+		
+	Scanner scan = new Scanner(System.in);
+	
+	System.out.println("Entre como um valor");
+	int numero = scan.nextInt();
+	System.out.println("O valor de entrada pé "+numero);
+	}
+	
 }
+
