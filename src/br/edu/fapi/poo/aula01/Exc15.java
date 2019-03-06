dfe6019bddae454fd5ecf5df16907c2169edf7d8
bruno <bruno.1301817@fapi-pinhais.edu.br>
2019-03-06 12:26:52 -0300

    Exc15

---
+++ src/br/edu/fapi/poo/aula01/Exc15.java
@@ -0,0 +1,21 @@
+package br.edu.fapi.poo.aula01;
+
+import java.util.Scanner;
+
+public class Exc15 {
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+        int[] numeros = new int[5];
+
+        System.out.println("Digite 5 numeros.");
+        System.out.println("------------------------");
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Digite "+(i+1)+"º numero.");
+            numeros[i] = ler.nextInt();
+        }
+        System.out.println("---------------------");
+        System.out.println("Numeros guaradado no vetor de 5 posições");
+        for (int auxFor :numeros)
+            System.out.println(auxFor);
+    }
+}
