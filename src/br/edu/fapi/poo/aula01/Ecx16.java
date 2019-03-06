f554b2ae947c771bc3793b24dc3cbf352d09b51b
bruno <bruno.1301817@fapi-pinhais.edu.br>
2019-03-06 12:35:11 -0300

    Exc16

---
+++ src/br/edu/fapi/poo/aula01/Ecx16.java
@@ -0,0 +1,23 @@
+package br.edu.fapi.poo.aula01;
+
+import java.util.Scanner;
+
+public class Ecx16 {
+    public static void main(String[] args) {
+
+        Scanner ler = new Scanner(System.in);
+        int[] numeros = new int[10];
+
+        System.out.println("Digite 10 numeros.");
+        System.out.println("------------------------");
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Digite "+(i+1)+"º numero.");
+            numeros[i] = ler.nextInt();
+        }
+        System.out.println("--------------------------------------");
+        for (int i = 9; i >=0 ; i--) {
+            System.out.println( (i+1) +"º numero guardado no vetor -> "+numeros[i]);
+
+        }
+    }
+}
