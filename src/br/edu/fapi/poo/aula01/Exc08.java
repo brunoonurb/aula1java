d777b0990734e99607f1a3c7a6b4e13f0cab43f4
bruno <bruno.1301817@fapi-pinhais.edu.br>
2019-03-06 10:32:28 -0300

    Exc08

---
+++ src/br/edu/fapi/poo/aula01/Exc08.java
@@ -0,0 +1,10 @@
+package br.edu.fapi.poo.aula01;
+
+public class Exc08 {
+    public static void main(String[] args) {
+        int cont=0;
+        while ( cont <15){
+            System.out.println("ola eu sou uma estrutura de repetição ("+(++cont)+"º)");
+        }
+    }
+}
