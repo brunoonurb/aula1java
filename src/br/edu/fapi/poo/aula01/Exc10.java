378a155d1a36b5b64861f970417a98baf4121243
bruno <bruno.1301817@fapi-pinhais.edu.br>
2019-03-06 11:05:38 -0300

    Exc10

---
+++ src/br/edu/fapi/poo/aula01/Exc10.java
@@ -0,0 +1,25 @@
+package br.edu.fapi.poo.aula01;
+
+import java.util.Scanner;
+
+public class Exc10 {
+    public static void main(String[] args) {
+
+        Scanner ler = new Scanner(System.in);
+        int soma = 0;
+        int numero = 0;
+
+        System.out.println("Digite um conjunto de numeros desejado.");
+        System.out.println("  Para FINALIZAR DIGITE |0|");
+        do{
+            System.out.println("--------------------");
+            System.out.print("Digite um numero -> ");
+            numero=ler.nextInt();
+            soma+=numero;
+        }while(numero!=0);
+        System.out.println("----------------------------------------");
+        System.out.println("A soma dos numeros digitados foi -> "+soma);
+        System.out.println("----------------------------------------");
+
+    }
+}
