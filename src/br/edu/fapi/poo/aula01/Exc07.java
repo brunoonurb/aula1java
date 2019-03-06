030cd410a75e9fef17b3b56d5ead2148e55cd300
bruno <bruno.1301817@fapi-pinhais.edu.br>
2019-03-06 10:26:26 -0300

    Exc07

---
+++ src/br/edu/fapi/poo/aula01/Exc07.java
@@ -0,0 +1,12 @@
+package br.edu.fapi.poo.aula01;
+
+public class Exc07 {
+    public static void main(String[] args) {
+
+    int i=0;
+    while ( i < 10){
+        System.out.println("-----------------------------------------------");
+        System.out.println("Ola eu sou uma estruturar de repetição |"+(++i));
+        }
+    }
+}
